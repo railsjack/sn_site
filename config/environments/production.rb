2e600f5b0b880ff8a56bf93b8818c01900a3a00c
Web Dev <jinnahrae@gmail.com>
2019-03-17 12:06:56 -0700

    Update email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,16 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: 'app.safetynotice.com',
+      # domain: 'app.safetynotice.com',
+      domain: 'safetynotice.herokuapp.com',
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: Rails.application.secrets.email_provider_username,
       password: Rails.application.secrets.email_provider_password
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { host: 'app.safetynotice.com' }
+  # config.action_mailer.default_url_options = { host: 'app.safetynotice.com' }
+  config.action_mailer.default_url_options = { host: 'safetynotice.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
