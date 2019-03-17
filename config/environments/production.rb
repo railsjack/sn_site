db99bb0df4603233ba7c4c0cd2aebf933a69317e
Web Dev <jinnahrae@gmail.com>
2019-03-17 11:08:29 -0700

    Updated assets compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # config.assets.compile = false
+  # For Heroku
   config.assets.compile = false
 
   # Generate digests for assets URLs.
